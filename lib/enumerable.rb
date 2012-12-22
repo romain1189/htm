0dd25d22971173f60b48bf839ef79ddf804a5617
Romain Franceschini <romain1189@gmail.com>
2012-12-22 20:29:43 +0100

    Monkey patch Enumerable to parallelize #each and #map methods

---
+++ lib/enumerable.rb
@@ -0,0 +1,13 @@
+module Enumerable
+  def pmap(&block)
+    futures = map { |elem| Celluloid::Future.new(elem, &block) }
+    futures.map { |future| future.value }
+  end
+  alias_method :pcollect, :pmap
+
+  def peach(&block)
+    futures = map { |elem| Celluloid::Future.new(elem, &block) }
+    futures.each { |future| future.value }
+    self
+  end
+end
---
+++ test/enumerable_test.rb
@@ -0,0 +1,29 @@
+require 'minitest/autorun'
+require 'celluloid'
+require 'enumerable'
+
+class TestEnumerable < MiniTest::Unit::TestCase
+  def setup
+    @ary = (1..25).to_a
+  end
+
+  def test_should_respond_to_monkey_patched_methods
+    assert_respond_to @ary, :pmap
+    assert_respond_to @ary, :peach
+  end
+
+  def test_pmap
+    proc = Proc.new { |n| n ** 2 }
+    assert_equal @ary.map(&proc), @ary.pmap(&proc)
+  end
+
+  def test_peach
+    expected = []
+    @ary.each { |n| expected << (n ** 2) }
+
+    act = []
+    @ary.peach { |n| act << (n ** 2) }
+
+    assert_equal expected, act
+  end
+end
