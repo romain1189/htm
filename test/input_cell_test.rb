bb72a0251f3d173a00e284b7eddfc2b3d1afb196
Romain Franceschini <franceschini.romain@gmail.com>
2013-01-04 02:48:41 +0100

    should have been part of previous commit

--- test/input_cell_test.rb
+++ test/input_cell_test.rb
@@ -12,11 +12,11 @@ class TestInputCell < MiniTest::Unit::TestCase
 
     @current_input = [0b0, 0b1, 0b1, 0b0, 0b0, 0b0, 0b1, 0b1, 0b0, 0b1, 0b0,
                       0b1, 0b1, 0b0, 0b1, 0b0, 0b0, 0b1, 0b0, 0b1, 0b0, 0b1,
-                      0b0, 0b1, 0b1, 0b1]
+                      0b0, 0b1, 0b1]
 
     @previous_input = [0b1, 0b0, 0b1, 0b0, 0b1, 0b0, 0b1, 0b0, 0b0, 0b1, 0b0,
                        0b1, 0b1, 0b1, 0b1, 0b0, 0b0, 0b1, 0b1, 0b0, 0b0, 0b1,
-                       0b0, 0b1, 0b0, 0b1]
+                       0b0, 0b1, 0b0]
 
     @mock.expect(:current_input,  @current_input)
     @mock.expect(:previous_input, @previous_input)
@@ -31,14 +31,14 @@ class TestInputCell < MiniTest::Unit::TestCase
   end
 
   def test_offset_value_should_be_row_major_order
-    assert_equal 19, @input.offset
+    assert_equal 23, @input.offset
   end
 
   def test_active
-    assert_equal (@current_input[19] == 1), @input.active?
+    assert_equal (@current_input[23] == 1), @input.active?
   end
 
   def test_was_active
-    assert_equal (@previous_input[19] == 1), @input.was_active?
+    assert_equal (@previous_input[23] == 1), @input.was_active?
   end
 end
