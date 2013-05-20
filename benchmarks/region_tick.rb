222531c668aaeebf4efd6a583fe6f6d2ddb73d69
Romain Franceschini <franceschini.romain@gmail.com>
2013-05-20 15:14:05 +0200

    removed array allocation from benchmark

--- benchmarks/region_tick.rb
+++ benchmarks/region_tick.rb
@@ -1,9 +1,15 @@
+$:.push File.expand_path('../../lib', __FILE__)
+
 require 'htm'
 require 'benchmark'
 
+n = 10
+
 region = HTM::Region.new
+data = n.times.map { Array.new(region.input_size) { rand.round } }
+
 Benchmark.bmbm do |x|
   x.report("iteration") {
-    region.tick(Array.new(region.input_size) { rand.round })
+    n.times { |i| region.tick(data[i]) }
   }
 end
